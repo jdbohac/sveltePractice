c8076e8c00d66388489568fd5ad2c2d2cf890d24
jdbohac <jeffrey.bohac@gmail.com>
2023-06-26 15:18:54 -0400

    learning svelte api requests

--- hunnybusapp/src/routes/+page.svelte
+++ hunnybusapp/src/routes/+page.svelte
@@ -1,19 +1,34 @@
 
         <script>
-        
+        import { onMount } from 'svelte'
 		import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
 		import { Datepicker } from 'svelte-calendar'
+		import axios from 'axios'
+		
+		let eventsArray = []
+		
 		let number = 0
-
+		
 		const clickCount = () => {
 	        number += 1
 		    return number
 		  }
+		  onMount(() => {
+		  fetch('http://localhost:3003/events').then(response => response.json).then(data => {
+			console.log(data)
+			eventsArray.set(data)
+		}).catch((error) => {
+			console.log(error)
+		})
+		  }, [])
 		</script>
 		<div id="logo">
 		<img class="" src="./hunnybuslogo.webp" alt="">
 		</div>
 		<button on:click={clickCount}>{number}</button>
+		{#each eventsArray as event}
+		<p>{event.name}</p>
+		{/each}
 		<Accordion>
 		    <AccordionItem open>
 		        <svelte:fragment slot="summary">Hello</svelte:fragment>
